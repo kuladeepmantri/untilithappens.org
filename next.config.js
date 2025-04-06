e470b013c665b9d38c8cc9f6f0f43c1560a603f0
Kuladeep Mantri <kuladeepmantri@gmail.com>
2025-04-06 16:42:31 -0400

    push update

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/untilithappens.org',
+}
+
+module.exports = nextConfig 
